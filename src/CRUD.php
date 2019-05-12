dce378dffea227a389e5a499344da08bb83ba7e2
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2019-05-12 17:48:49 +0530

    inits has to be in model otherwise not effecting on reload as looks like recursive render is not get called when jsReloaded CRUD

--- src/CRUD.php
+++ src/CRUD.php
@@ -1,50 +1,42 @@
 <?php
 
-
 namespace atk4\acl;
-
 
 class CRUD extends \atk4\ui\CRUD {
 
-    protected $actionDecorator = '\atk4\acl\TableColumn\AclEditDelete';
-    protected $acl_controller = null;
-    public $auth_model = null;
-    public $db = null;
+	protected $actionDecorator = '\atk4\acl\TableColumn\AclEditDelete';
+	protected $acl_controller = null;
+	public $auth_model = null;
+	public $db = null;
 
-	public function setModel(\atk4\data\Model $m, $defaultFields = null)
-    {
-        if ($defaultFields !== null) {
-            $this->fieldsDefault = $defaultFields;
-        }
+	public function setModel(\atk4\data\Model $m, $defaultFields = null) {
+		if ($defaultFields !== null) {
+			$this->fieldsDefault = $defaultFields;
+		}
 
-        \atk4\ui\Grid::setModel($m, $this->fieldsRead ?: $this->fieldsDefault);
-        $this->model->unload();
+		\atk4\ui\Grid::setModel($m, $this->fieldsRead ?: $this->fieldsDefault);
+		$this->model->unload();
 
-        $this->acl_controller = $this->add(['\atk4\acl\Controller\Acl','auth_model'=>$this->auth_model,'db'=>$this->db]);
-        return $this->model;
-    }
+		$this->acl_controller = $this->add(['\atk4\acl\Controller\Acl', 'auth_model' => $this->auth_model, 'db' => $this->db]);
+		if ($this->canCreate) {
+			$this->initCreate();
+		}
 
-    function recursiveRender(){
-        if ($this->canCreate) {
-            $this->initCreate();
-        }
+		if ($this->canUpdate) {
+			$this->initUpdate();
+		}
 
-        if ($this->canUpdate) {
-            $this->initUpdate();
-        }
+		if ($this->canDelete) {
+			$this->initDelete();
+		}
+		return $this->model;
+	}
 
-        if ($this->canDelete) {
-            $this->initDelete();
-        }
-        return parent::recursiveRender();
-    }
+	public function addAction($button, $action, $confirm = false) {
+		if (!$this->actions) {
+			$this->actions = $this->table->addColumn('actions', new $this->actionDecorator($this->acl_controller->status_actions, $this->acl_controller));
+		}
 
-    public function addAction($button, $action, $confirm = false)
-    {
-        if (!$this->actions) {
-            $this->actions = $this->table->addColumn('actions', new $this->actionDecorator($this->acl_controller->status_actions, $this->acl_controller));
-        }
-
-        return $this->actions->addAction($button, $action, $confirm);
-    }
+		return $this->actions->addAction($button, $action, $confirm);
+	}
 }
